7a616d349ef262f020d76db8427519a9ec04366b
lx0720 <liuxing0720@qq.com>
2022-07-25 11:20:31 +0800

    版本2.0

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    private string git = "1.0";
+    private string git = "2.0";
     // Start is called before the first frame update
     void Start()
     {
